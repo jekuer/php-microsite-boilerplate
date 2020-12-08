22ffd01c3a07be765c850fdb19ae65e81954c9a8
Jens Kuerschner <mail@jenskuerschner.de>
2020-12-08 19:15:37 +0100

    Fix typos and translation of AMP cookie banner

--- templates/cookie_consent_amp.php
+++ templates/cookie_consent_amp.php
@@ -19,31 +19,19 @@
       <div class="dismiss-button" role="button" tabindex="0" on="tap:myUserConsent.dismiss">
         [ X ]
       </div>
-      <h2>Cookies.</h2>
-      <?php if ($language['active'] == 'de') { ?>
-        <p>
-          <strong>Diese Webseite nutzt Cookies.</strong><br>
-          Manche Cookies sind nötig, um die Kernfunktionalität zu gewährleisten. Diese sind automatisch aktiviert. Analytische Cookies helfen dabei, die Seite weiter zu optimieren. Hierbei werden anonymisierte Daten gesammelt und gespeichert.
-        </p>
-        <p>
-          <strong>Bitte akzeptiere diese analytischen Cookies oder lehne sie ab.</strong>
-        </p>
-        <button on="tap:myUserConsent.reject" class="ampstart-btn-small">Ablehnen</button>
-        <button on="tap:myUserConsent.accept" class="ampstart-btn">Akzeptieren</button>
-      <?php } else { ?>
-        <p>
-          <strong>This website uses cookies.</strong><br>
-          Some cookies are required to provide core functionality. The website won't function properly without these cookies and they are enabled by default. Analytical cookies help to improve this website by collecting and reporting anonymized information on its usage.
-        </p>
-        <p>
-          <strong>Please accept or reject analytical cookies.</strong>
-        </p>
-        <button on="tap:myUserConsent.reject" class="ampstart-btn-small">Reject</button>
-        <button on="tap:myUserConsent.accept" class="ampstart-btn">Accept</button>
-      <?php } ?>
+      <h2><?php echo T_('Cookies'); ?>.</h2>
+      <p>
+        <strong><?php echo T_('This website uses cookies'); ?>.</strong><br>
+        <?php echo T_('Some cookies are required to provide core functionality. The website won\'t function properly without these cookies and they are enabled by default. Analytical cookies help to improve this website by collecting and reporting anonymized information on its usage.'); ?>
+      </p>
+      <p>
+        <strong><?php echo T_('Please accept or reject analytical cookies'); ?>.</strong>
+      </p>
+      <button on="tap:myUserConsent.reject" class="ampstart-btn-small"><?php echo T_('Reject'); ?></button>
+      <button on="tap:myUserConsent.accept" class="ampstart-btn"><?php echo T_('Accept'); ?></button>
     </div>
   </div>
   <div id="post-consent-ui">
-    <button on="tap:myUserConsent.prompt()" class="ampstart-btn">Cookie Settings</button>
+    <button on="tap:myUserConsent.prompt()" class="ampstart-btn"><?php echo T_('Cookie Settings'); ?></button>
   </div>
 </amp-consent>

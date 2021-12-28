35301292f09128bc8ad671478298fd9bd9de28b3
Jens Kuerschner <mail@jenskuerschner.de>
2021-12-28 17:39:17 +0100

    update button on error pages

--- pages/error.php
+++ pages/error.php
@@ -2,5 +2,5 @@
 <h1 itemprop="headline"><?php echo T_('404'); ?></h1>
 <h2><?php echo T_('Unfortunately, the requested page is not available.'); ?></h2>
 <p>&nbsp;</p>
-<a href="<?php echo $the_page_url_full; ?>" class="button">&larr; <?php echo T_('back to home'); ?></a>
+<a href="<?php echo $the_page_url_full; ?>" class="text-white no-underline bg-primary border-0 py-2 px-6 focus:outline-none hover:bg-gray-600 hover:text-white hover:shadow-md rounded-lg">&larr; <?php echo T_('back to home'); ?></a>
 <p>&nbsp;</p>

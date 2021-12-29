7309ab1dbbc42b54cc6daa6cfe01c6c5e5833914
Jens Kuerschner <mail@jenskuerschner.de>
2021-12-29 13:25:10 +0100

    error and offline pages fix

--- pages/offline.php
+++ pages/offline.php
@@ -1,4 +1,4 @@
 
-<h1 itemprop="headline"><?php echo T_('Offline'); ?></h1>
+<h1 itemprop="headline" class="font-semibold text-lg pb-16"><?php echo T_('Offline'); ?></h1>
 <h2><?php echo T_('You are offline. Please check your internet connection and try again.'); ?></h2>
 

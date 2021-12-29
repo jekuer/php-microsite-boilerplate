ce3c3dfa5d308140efc73e1664ca66e8788c32c1
Jens Kuerschner <mail@jenskuerschner.de>
2021-12-29 13:37:19 +0100

    more dummy page fixes

--- pages/error.php
+++ pages/error.php
@@ -1,6 +1,4 @@
 
-<h1 itemprop="headline" class="font-semibold text-lg pb-16"><?php echo T_('404'); ?></h1>
-<h2><?php echo T_('Unfortunately, the requested page is not available.'); ?></h2>
-<p>&nbsp;</p>
-<a href="<?php echo $the_page_url_full; ?>" class="text-white no-underline bg-primary border-0 py-2 px-6 focus:outline-none hover:bg-gray-600 hover:text-white hover:shadow-md rounded-lg">&larr; <?php echo T_('back to home'); ?></a>
-<p>&nbsp;</p>
+<h1 itemprop="headline" class="font-semibold text-2xl pb-16"><?php echo T_('404'); ?></h1>
+<h2 class="pb-20"><?php echo T_('Unfortunately, the requested page is not available.'); ?></h2>
+<a href="<?php echo $the_page_url_full; ?>" class="text-white no-underline bg-primary border-0 my-6 py-2 px-6 focus:outline-none hover:bg-gray-600 hover:text-white hover:shadow-md rounded-lg">&larr; <?php echo T_('back to home'); ?></a>
--- pages/offline.php
+++ pages/offline.php
@@ -1,4 +1,4 @@
 
-<h1 itemprop="headline" class="font-semibold text-lg pb-16"><?php echo T_('Offline'); ?></h1>
-<h2><?php echo T_('You are offline. Please check your internet connection and try again.'); ?></h2>
+<h1 itemprop="headline" class="font-semibold text-2xl pb-16"><?php echo T_('Offline'); ?></h1>
+<h2 class="pb-20"><?php echo T_('You are offline. Please check your internet connection and try again.'); ?></h2>
 

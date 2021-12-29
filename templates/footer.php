c50b0411aacdfd7c87537e263e9e18594c73c012
Jens Kuerschner <mail@jenskuerschner.de>
2021-12-29 17:09:14 +0100

    fix

--- templates/footer.php
+++ templates/footer.php
@@ -2,11 +2,11 @@
     </div>
     
     <footer class="p-8 text-sm">
-      <p class="text-gray-600 border-gray-600">
+      <p class="text-gray-600">
         <a class="p-4 font-normal no-underline" href="<?php echo $the_page_url_full . $pages[$language['active']]['legal-notice']['slug']; ?>/"><?php echo $pages[$language['active']]['legal-notice']['name']; ?></a>
         <a class="p-4 font-normal no-underline" href="<?php echo $the_page_url_full . $pages[$language['active']]['privacy-policy']['slug']; ?>/"><?php echo $pages[$language['active']]['privacy-policy']['name']; ?></a>
         <?php echo create_language_switcher($the_page->id); ?>
-        <span class="block sm:inline-block py-4 sm:py-0"><?php echo create_language_switcher($the_page->id); ?></span>
+        <span class="block sm:inline-block py-4 sm:py-0 border-gray-600"><?php echo create_language_switcher($the_page->id); ?></span>
       </p>
       <p class="p-8 text-gray-500">&copy; <?php echo date('Y'); ?></p>
     </footer>

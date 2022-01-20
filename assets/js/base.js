6090242ef8097d1899c4efccf0f413a989244747
Jens Kuerschner <mail@jenskuerschner.de>
2022-01-20 15:53:31 +0100

    add secure flag to setCookie JS

--- assets/js/base.js
+++ assets/js/base.js
@@ -22,7 +22,7 @@ function setCookie(cname, cvalue, exdays) {
   var d = new Date();
   d.setTime(d.getTime() + (exdays*24*60*60*1000));
   var expires = "expires="+ d.toUTCString();
-  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/;secure";
 }
 
 // Scroll to Section.

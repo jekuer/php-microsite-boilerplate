fffbfd9f914b6de7c355050d121c407b1fcc07b3
Jens Kuerschner <mail@jenskuerschner.de>
2022-04-29 20:13:32 +0200

    security patch

--- config.php
+++ config.php
@@ -6,7 +6,7 @@
 
 // Current version of the website.
 // Will be used to control css and js caching.
-$version_nr = '2.0.9';
+$version_nr = '2.0.10';
 
 // Multilanguage
 $language['default'] = 'en'; // Use a key from the array below.

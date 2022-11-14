e68fd804230f089af21085002c33721da82dbb4a
Jens Kuerschner <mail@jenskuerschner.de>
2022-11-14 18:40:37 +0100

    dependency fix

--- config.php
+++ config.php
@@ -6,7 +6,7 @@
 
 // Current version of the website.
 // Will be used to control css and js caching.
-$version_nr = '2.0.11';
+$version_nr = '2.0.12';
 
 // Multilanguage
 $language['default'] = 'en'; // Use a key from the array below.
